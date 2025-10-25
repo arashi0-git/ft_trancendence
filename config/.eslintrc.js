033354e753560b8fa9a110b2d6ddf502f9fdf895
Yuhi <mraon0620inbabb@gmail.com>
2025-10-25 15:19:00 +0900

    chore: repair tsconfig and lint setup for split backend/frontend

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,37 +1,46 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname + '/..',
-    sourceType: 'module',
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname + "/..",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'unused-imports'],
-  extends: [
-    'eslint:recommended',
-  ],
+  plugins: ["@typescript-eslint", "unused-imports"],
+  extends: ["eslint:recommended"],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['dist/**/*', 'coverage/**/*', 'node_modules/**/*', 'config/**/*'],
+  ignorePatterns: [
+    "dist/**/*",
+    "coverage/**/*",
+    "node_modules/**/*",
+    "config/**/*",
+  ],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { 
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_',
-      caughtErrorsIgnorePattern: '^_'
-    }],
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': ['error', { 
-      vars: 'all', 
-      varsIgnorePattern: '^_', 
-      args: 'after-used', 
-      argsIgnorePattern: '^_' 
-    }],
-    'no-unused-vars': 'off',
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "no-unused-vars": "off",
   },
 };
